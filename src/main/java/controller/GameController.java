f460c1d0fe6012a75ca6753c55fcd0b4e27d6c62
Savitar97 <atoth1571@gmail.com>
2020-04-09 16:24:07 +0200

    modify scene size,adding gaming scene,get the user name

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.awt.*;
+
+public class GameController {
+    private String nickName;
+    @FXML
+    private Label nicknameLabel;
+
+    public void setNickname(String nickName) {
+    this.nickName=nickName;
+        nicknameLabel.setText("Hi,"+nickName+"!");
+        System.out.println(this.nickName);
+    }
+}
--- src/main/java/controller/LauncherController.java
+++ src/main/java/controller/LauncherController.java
@@ -1,25 +1,37 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+
 public class LauncherController {
+    @FXML
+    public Button runButton;
     @FXML
     private Label errorLabel;
     @FXML
     private TextField nicknameTextfield;
-    public void runAction(ActionEvent actionEvent) {
+    public void runAction(ActionEvent actionEvent) throws IOException {
         if (nicknameTextfield.getText().isEmpty()) {
             errorLabel.setText("* Username is empty!");
+        }
+        else {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+            Parent root = fxmlLoader.load();
+            fxmlLoader.<GameController>getController().setNickname(nicknameTextfield.getText());
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
         }
     }
 }
