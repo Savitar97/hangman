817410fde9f7952e6d6a74e13af3323958746569
Savitar97 <atoth1571@gmail.com>
2020-04-13 05:08:29 +0200

    Make a game prototype,tried something

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,21 +1,13 @@
 package controller;
 
 import game.Game;
-import game.RandomWord;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import java.awt.*;
+
 import java.util.List;
 import java.util.Arrays;
 
@@ -49,13 +41,19 @@ public class GameController {
                 new Image(getClass().getResource("/images/hangman10.jpg").toExternalForm())
         );
         game=new Game();
-        word.setText(game.getWord());
+        word.setText(viewEncrypted(game.getEncryptedWord()));
+        gallows.setImage(gallowImages.get(0));
     }
-
+    private String viewEncrypted(String ve){
+        String sb="";
+        for (int i = 0; i < ve.length(); i++) {
+            sb+=ve.charAt(i)+" ";
+        }
+        return sb;
+    }
     public void setNickname(String nickName) {
     this.nickName=nickName;
         nicknameLabel.setText("Hi,"+nickName+"!");
-        gallows.setImage(new Image(String.valueOf(getClass().getResource("/images/hangman1.jpg"))));
         System.out.println(this.nickName);
     }
 
@@ -63,13 +61,28 @@ public class GameController {
     themeName=""+value;
     }
 
-    public void getChoice(ActionEvent actionEvent) {
+    public void getChoice(ActionEvent actionEvent) throws Exception {
         Button btn=(Button)actionEvent.getSource();
         String choice=btn.getText();
-        System.out.println(choice);
+        game.makeaGuess(btn.getText());
         btn.setVisible(false);
+        word.setText(viewEncrypted(game.getEncryptedWord()));
         score.setText(""+game.getScore());
-        game=new Game();
-        word.setText(game.getWord());
+        setGallowImages(game.getCountFault());
+
+    }
+    public void setGallowImages(int i){
+        switch (game.getCountFault()){
+            case 1:gallows.setImage(gallowImages.get(1)); break;
+            case 2:gallows.setImage(gallowImages.get(2)); break;
+            case 3:gallows.setImage(gallowImages.get(3)); break;
+            case 4:gallows.setImage(gallowImages.get(4)); break;
+            case 5:gallows.setImage(gallowImages.get(5)); break;
+            case 6:gallows.setImage(gallowImages.get(6)); break;
+            case 7:gallows.setImage(gallowImages.get(7)); break;
+            case 8:gallows.setImage(gallowImages.get(8)); break;
+            case 9:gallows.setImage(gallowImages.get(9)); break;
+            default:throw new StackOverflowError();
+        }
     }
 }
--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,15 +1,19 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class Game {
-    private static final int score=0;
+    private static int score=0;
     private String word;
+    private String encryptedWord;
     private static ArrayList<String> usedWords=new ArrayList<>();
     private ArrayList<Character> usedLetters;
-
+    private final int MAX_FAULT=9;
+    private int countFault=0;
     public Game() {
         word=wordSelector();
+        encryptedWord=setEncryptedWord(word);
         usedLetters=new ArrayList<>();
     }
     private String wordSelector(){
@@ -18,13 +22,66 @@ public class Game {
             usedWords.add(temp);
             return temp;
         }
-        else return wordSelector();
+        else{
+            return wordSelector();
+        }
+    }
+
+    private boolean checkIsInAlphabet(char c){
+        if((c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z')){
+            return true;
+        }
+        else {
+            throw new NoSuchElementException();
+        }
+    }
+    private String setEncryptedWord(String w){
+        String ew = "";
+        for (int i = 0; i <word.length() ; i++) {
+            ew+="_";
+        }
+        return ew;
+    }
+
+    public void makeaGuess(String w) throws Exception {
+        char searchHelp=w.toUpperCase().charAt(0);
+        char[] wordArray=word.toCharArray();
+        StringBuilder sb=new StringBuilder(encryptedWord);
+        if(checkIsInAlphabet(searchHelp)){
+            if (!usedLetters.contains(searchHelp)){
+                usedLetters.add(searchHelp);
+                boolean find=false;
+                for (int i = 0; i <wordArray.length ; i++) {
+                    if (wordArray[i]==searchHelp){
+                        score+=100;
+                        sb.setCharAt(i,searchHelp);
+                        find=true;
+                    }
+                }
+                if (find==false){
+                    countFault++;
+                }
+
+            }
+            else {
+                throw new Exception("LetterWasUsed");
+            }
+        }
+        encryptedWord=sb.toString();
+        System.out.println(encryptedWord);
     }
     public String getWord() {
         return word;
     }
-
     public static int getScore() {
         return score;
+    }
+
+    public String getEncryptedWord() {
+        return encryptedWord;
+    }
+
+    public int getCountFault() {
+        return countFault;
     }
 }
